e842d3bc5004c7b44343100ac3029166b8ef02d2
StefanIordache <iordachestefan1@gmail.com>
2018-04-15 23:12:34 +0300

    ProjectPlanner main method

---
+++ src/ProjectPlanner.java
@@ -0,0 +1,13 @@
+import Controllers.LoginMenuController;
+import Controllers.ProjectsController;
+import Controllers.ProjectsMenuController;
+import Utils.*;
+
+public class ProjectPlanner {
+    
+    public static void main(String[] args) {
+        
+        LoginMenuController controller = new LoginMenuController();   
+        
+    }
+}
