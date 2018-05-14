b7dfbc8ef2e991b8dee218e9f5a1caf015a73c4d
StefanIordache <iordachestefan1@gmail.com>
2018-05-14 21:08:28 +0300

    UsersController client

--- ProjectPlanner_Client/src/Controllers/UsersController.java
+++ ProjectPlanner_Client/src/Controllers/UsersController.java
@@ -1,25 +1,25 @@
 package Controllers;
 
+import Client.Client;
 import Models.*;
 import Utils.*;
 import java.sql.*;
 import java.util.Date;
 import java.util.List;
 import java.util.ArrayList;
+import javax.sql.rowset.CachedRowSet;
 
 public class UsersController {
-    
-    private static final Connection conn = MySQLConnector.getConnection();
     
     public static List<User> GetAll() {
         
         List<User> users = new ArrayList<>();
         
         try {
-            String query = "SELECT * FROM users";
+            Client.sendString("Users");
+            Client.sendString("getAll");
             
-            Statement statement = conn.createStatement();
-            ResultSet result = statement.executeQuery(query);
+            CachedRowSet result = (CachedRowSet) Client.receiveObject();
             
             while (result.next()) {
                 User user = new User(result.getInt("Id"),
@@ -30,8 +30,6 @@ public class UsersController {
                                     result.getString("LastName"));
                 users.add(user);
             }
-            
-            statement.close();
         }
         catch (Exception e) {
             System.out.println("Error: " + e);
@@ -45,11 +43,10 @@ public class UsersController {
         List<User> managers = new ArrayList<>();
         
         try {
-            String query = "SELECT * FROM users u JOIN roles r ON u.RoleId = r.Id " + 
-                            "WHERE UPPER(r.Title) = 'MANAGER'";
+            Client.sendString("Users");
+            Client.sendString("getManagers");
             
-            Statement statement = conn.createStatement();
-            ResultSet result = statement.executeQuery(query);
+            CachedRowSet result = (CachedRowSet) Client.receiveObject();
             
             while (result.next()) {
                 User manager = new User(result.getInt("Id"),
@@ -59,9 +56,7 @@ public class UsersController {
                                     result.getString("FirstName"),
                                     result.getString("LastName"));
                 managers.add(manager);
-            }
-            
-            statement.close();          
+            }        
         } 
         catch (Exception e) {
             System.out.println("Error: " + e);
